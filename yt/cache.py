157ec703e734b7dbf770cfb74fff683d8bd46f85
Niakr1s <pavel2188@gmail.com>
2022-10-05 15:35:44 +0300

    added SqlCache

--- yt/cache.py
+++ yt/cache.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+import sqlite3
 
 
 class Cache(metaclass=ABCMeta):
@@ -24,8 +25,44 @@ class MemoryCache(Cache):
     def add_items(self, items: list):
         self.cache.update(items)
 
-    def is_in_cache(self, item) -> bool:
-        return item in self.cache
+
+class SqliteCache(Cache):
+    def __init__(self, path: str):
+        self.con = sqlite3.connect(path)
+        self.cur = self.con.cursor()
+
+    def filter_uncached(self, items: list) -> list:
+        in_str = ', '.join(["?"]*len(items))
+        exec = self.cur.execute(
+            f'SELECT item FROM items WHERE item IN ({in_str});', items)
+        cached = set([item[0] for item in exec.fetchall()])
+        uncached = set(items).difference(cached)
+        return uncached
 
     def add_items(self, items: list):
-        self.cache.update(items)
+        self.cur.executemany('INSERT OR IGNORE INTO "items" ("item") VALUES (?);', [
+            [item] for item in items])
+
+        self.con.commit()
+
+    def create(path: str):
+        con = sqlite3.connect(path)
+        con.execute(
+            "CREATE TABLE items(item varchar(50) UNIQUE NOT NULL);")
+
+
+if __name__ == "__main__":
+    # some tests for SqliteCache
+    import pathlib
+
+    db = pathlib.Path("d:/Projects/ytldl/tmp/.ytldl/ytldl.db")
+    db.parent.mkdir(exist_ok=True, parents=True)
+    try:
+        SqliteCache.create(db)
+    except Exception:
+        pass
+
+    cache = SqliteCache(str(db))
+    cache.add_items([str(i) for i in range(10)])
+    uncached = cache.filter_uncached([str(i+5) for i in range(10)])
+    print(uncached)
