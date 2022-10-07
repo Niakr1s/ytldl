d8d03379952ceeeb449e6ec78791de6431c65f8f
Niakr1s <pavel2188@gmail.com>
2022-10-07 14:55:41 +0300

    added consts.py

--- ytldl/tests/test_postprocessors.py
+++ ytldl/tests/test_postprocessors.py
@@ -1,25 +1,23 @@
 import pathlib
 import shutil
 import unittest
+from ytldl.tests import consts
 from ytldl.yt.postprocessors import FilterPP, FilterPPException, LyricsPP, MetadataPP
 
 
 class TestLyricsPP(unittest.TestCase):
-    VIDEO_ID_WITH_LYRICS = "pm9JyMiAU6A"
-    VIDEO_ID_WITHOUT_LYRICS = "6qTyEOAWAOQ"
-
     def setUp(self) -> None:
         self.info = dict(id="changeme")
         self.lyrics_pp = LyricsPP()
 
     def test_with_lyrics(self):
-        self.info["id"] = self.VIDEO_ID_WITH_LYRICS
+        self.info["id"] = consts.VIDEO_ID_WITH_LYRICS
         _, info = self.lyrics_pp.run(self.info)
         self.assertIn("lyrics", info)
         self.assertTrue(info["lyrics"])
 
     def test_without_lyrics(self):
-        self.info["id"] = self.VIDEO_ID_WITHOUT_LYRICS
+        self.info["id"] = consts.VIDEO_ID_WITHOUT_LYRICS
         _, info = self.lyrics_pp.run(self.info)
         self.assertIn("lyrics", info)
         self.assertFalse(info["lyrics"])
