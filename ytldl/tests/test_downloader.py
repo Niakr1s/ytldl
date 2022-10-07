6587f19a32c29e7fb3ce37c7719a964d3c4aee87
Niakr1s <pavel2188@gmail.com>
2022-10-07 15:41:14 +0300

    added TestDownloader.test_download tests

---
+++ ytldl/tests/test_downloader.py
@@ -0,0 +1,37 @@
+import pathlib
+import shutil
+import unittest
+from ytldl.tests import consts
+from ytldl.yt.postprocessors import FilterPPException
+from ytldl.yt.download import Downloader
+
+
+class TestDownloader(unittest.TestCase):
+    def setUp(self) -> None:
+        self.dir = pathlib.Path("tmp/test")
+        if self.dir.exists():
+            shutil.rmtree(self.dir)
+        self.dir.mkdir(parents=True, exist_ok=True)
+
+    def test_track_download_song_with_lyrics(self):
+        d = Downloader(str(self.dir))
+        d.download_track(consts.VIDEO_ID_WITH_LYRICS)
+        self.assertTrue(len(list(self.dir.iterdir())) == 1)
+
+    def test_track_download_song_without_lyrics(self):
+        d = Downloader(str(self.dir))
+        d.download_track(consts.VIDEO_ID_WITHOUT_LYRICS)
+        self.assertTrue(len(list(self.dir.iterdir())) == 1)
+
+    def test_track_download_video(self):
+        d = Downloader(str(self.dir))
+        self.assertRaises(FilterPPException,
+                          d.download_track, consts.VIDEO_ID_VIDEO)
+        self.assertTrue(len(list(self.dir.iterdir())) == 0)
+
+    def tearDown(self):
+        shutil.rmtree(self.dir)
+
+
+if __name__ == '__main__':
+    unittest.main()
